b5b3a694a346339273098b044b138e0c9edac7d2
Frederico Silva Guimaraes <frederico.guimaraes@blive-ti.com.br>
2025-05-15 15:55:18 -0300

    https://intranet.mindsatwork.com.br/jira/browse/MSQ-210

--- image-overlap-ui/src/app/app.module.ts
+++ image-overlap-ui/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { CompareComponent } from './compare/compare.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    UploadComponent,
+    CompareComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
