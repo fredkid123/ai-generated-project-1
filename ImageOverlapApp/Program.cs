8cfd2d56d5a74348f55102790e775198f2e00424
Frederico Silva Guimaraes <frederico.guimaraes@blive-ti.com.br>
2025-05-15 11:23:15 -0300

    https://intranet.mindsatwork.com.br/jira/browse/MSQ-210

--- ImageOverlapApp/Program.cs
+++ ImageOverlapApp/Program.cs
@@ -1,9 +1,18 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar Serilog antes de qualquer outra coisa
+Log.Logger = new LoggerConfiguration()
+	.ReadFrom.Configuration(builder.Configuration)
+	.CreateLogger();
+
+builder.Host.UseSerilog();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IImageComparisonService, ImageComparisonService>();
-builder.Services.AddSingleton<IImageStorageService, ImageStorageService>();
 
 var app = builder.Build();
 
@@ -11,4 +20,17 @@ app.UseStaticFiles();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapControllers();
-app.Run();
+
+try
+{
+	Log.Information("Iniciando o aplicativo...");
+	app.Run();
+}
+catch (Exception ex)
+{
+	Log.Fatal(ex, "Erro fatal na inicialização");
+}
+finally
+{
+	Log.CloseAndFlush();
+}
