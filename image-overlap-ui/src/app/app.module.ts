a38adf88b04a653b28ad3a2b7d3b943bd3d61621
Frederico Silva Guimaraes <frederico.guimaraes@blive-ti.com.br>
2025-05-16 14:10:15 -0300

    https://intranet.mindsatwork.com.br/jira/browse/MSQ-210

--- image-overlap-ui/src/app/app.module.ts
+++ image-overlap-ui/src/app/app.module.ts
@@ -8,12 +8,36 @@ import { UploadComponent } from './uploadty/upload.component';
 import { CompareComponent } from './compare/compare.component';
 import { ErrorInterceptor } from './error.interceptor';
 
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
-	declarations: [AppComponent, UploadComponent, CompareComponent],
-	imports: [BrowserModule, FormsModule, HttpClientModule],
-	providers: [
-		{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
-	],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    UploadComponent,
+    CompareComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatInputModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
